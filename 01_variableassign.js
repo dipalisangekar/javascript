2d10828779c2942a27fda79d184ea5e4035c74d7
dipalisangekar <dipalisangekar@gmail.com>
2023-12-26 19:35:31 +0530

    added Variable assign
    added Variable assign

---
+++ 01_variableassign.js
@@ -0,0 +1,32 @@
+var myFullName;// undefined
+console.log(myFullName);
+
+myFullName = "Dipali Sangekar";
+console.log("myName:",myFullName);
+
+myFullName = "Dipali Nagendra Sangekar";
+console.log("myFullName:",myFullName);
+
+myLove = "I love only JS";
+console.log("myLove:",myLove);
+
+myname ="Dipali";
+console.log("firstName:",myname);
+
+myname ="Sangekar";
+console.log("lastName:",myname);
+
+myname ="33";
+console.log("age:",myname);
+
+myname ="Bengaluru";
+console.log("address:",myname);
+
+myname ="12345";
+console.log("pincode:",myname);
+
+myname ="Female";
+console.log("gender:",myname);
+
+myname ="Married";
+console.log("isMarried:",myname);
---
+++ helloWord.js
@@ -0,0 +1,17 @@
+ console.log("Hello js, I am Dipali");
+ var num="10";
+ console.log(num);
+ console.log("Dipali")
+ 
+ var myname; //Variable Intialization
+ console.log(myname);// undefined
+ 
+ var myname= "dipali";// Variable Declaration
+ console.log("myname");
+ console.log("First name",myname)
+
+ myname="Dipali Sangekar"// variable Update
+ console.log("full name:",myname)
+ 
+ var age="32";//Variable Declaration and Intialization
+ console.log("Age:",age);
